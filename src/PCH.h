bed9e953d408414357e055ac3ab20a0139285711
Caelvanost <caelvanost@gmail.com>
2026-08-29 16:44:29 +0200

    fix: enable std string-view literals for generated plugin source

--- src/PCH.h
+++ src/PCH.h
@@ -2,9 +2,13 @@
 
 #include <mutex>
 #include <optional>
+#include <string>
+#include <string_view>
 #include <unordered_set>
 
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 #include "SKSE/API.h"
 #include "SKSE/Events.h"
+
+using namespace std::literals;
